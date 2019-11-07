2c202b3737203090528975c2d3af1c0c95b92ea2
JS-jopepe <jovenel.s@outlook.com>
2019-11-07 20:04:27 -0300

    Criar uma pasta para a aula09j

---
+++ sapp/capp/script.js
@@ -0,0 +1,22 @@
+// CONSTRUIR UMA ESCADA DE 5 DEGRAUS NO CONSOLE
+// O MATERIAL DEVE SER #
+
+// #
+// ##
+// ###
+// ####
+// #####
+
+let material = prompt("Qual material você quer?");
+let quant_degraus = Number(prompt("Quantos degarus você quer?"));
+let array_degraus = [];
+let valor_indice = "";
+
+for(let i = 0; indice < quant_degraus; i++){
+    console.log(array_degraus[i]);
+}
+
+for(let i = 0; indice < quant_degraus; i++){
+    valor_indice = valor_indice + material;
+    array_degraus.push(valor_indice);
+}
